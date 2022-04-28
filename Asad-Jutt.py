7ae6b82055b6115c3d7540a5c1a0a66406e256ad
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2022-04-28 06:00:01 +0500

    Update Asad-Jutt.py

--- Asad-Jutt.py
+++ Asad-Jutt.py
@@ -1,17 +1,16 @@
 import os, platform
+ 
 try:
-    import requests
+   import requests
+ 
 except:
-    os.system('pip install requests')
+   os.system('pip2 install requests')
+ 
 import requests
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
     from Asad import check
     check()
 elif bit == '32bit':
-    from Ali import check
-    check()
-else:
-    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
-    os.system('exit')
- 
+    print('YOUR PHONE IS NOT SUPPORTED BRO')
