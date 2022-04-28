3d9cc6c63f1c0de1cdad51da1f64f19c7e520c0d
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2022-04-28 06:21:25 +0500

    Update Asad-Jutt.py

--- Asad-Jutt.py
+++ Asad-Jutt.py
@@ -1,2 +1,16 @@
-from Asad import cheak
-cheak()
+import os, platform
+ 
+try:
+   import requests
+ 
+except:
+   os.system('pip2 install requests')
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from JuttBrother import Asad
+    Asad()
+elif bit == '32bit':
+    print('YOUR PHONE IS NOT SUPPORTED BRO')
