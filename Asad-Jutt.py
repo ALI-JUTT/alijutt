c6baa0d797f40241359316f9fcbcfe80f92a862f
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2022-04-28 05:26:45 +0500

    Update Asad-Jutt.py

--- Asad-Jutt.py
+++ Asad-Jutt.py
@@ -6,11 +6,11 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from Asad import asad
-    asad()
+    from Asad import check
+    check()
 elif bit == '32bit':
-    from Ali import asad
-    asad()
+    from Ali import check
+    check()
 else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
     os.system('exit')
